c0c0c9d778a9002eabccc00311d7a12ca8c66061
Daniel R. Neal <dan.r.neal@gmail.com>
2020-08-06 13:45:52 -0700

    style: Move func to top of file

--- designs.js
+++ designs.js
@@ -1,27 +1,12 @@
-const colorPicker = document.querySelector('#colorPicker');
-let color = document.querySelector('#colorPicker').value;
-colorPicker.addEventListener('input', function() {
-    color = document.querySelector('#colorPicker').value;
-});
-
-const sizePicker = document.querySelector('#sizePicker');
-sizePicker.addEventListener('submit', function(event) {
-    event.preventDefault();
-    const height = sizePicker.height.value;
-    const width = sizePicker.width.value;
-    eraseGrid();
-    makeGrid(height, width);
-});
-
 /**
  * @description Erases any elements from an existing grid
  */
 function eraseGrid() {
-    const pixelCanvas = document.querySelector('#pixelCanvas');
-    const allRows = pixelCanvas.querySelectorAll('tr');
-    for (let i = 0; i < allRows.length; i++) {
-        allRows[i].remove();
-    }
+  const pixelCanvas = document.querySelector('#pixelCanvas');
+  const allRows = pixelCanvas.querySelectorAll('tr');
+  for (let i = 0; i < allRows.length; i++) {
+    allRows[i].remove();
+  }
 }
 
 /**
@@ -30,21 +15,34 @@ function eraseGrid() {
  * @param {number} width - number of columns in the grid
  */
 function makeGrid(height, width) {
-
-    const pixelCanvas = document.querySelector('#pixelCanvas');
-    pixelCanvas.addEventListener('click', function(event){
-        if (event.target.nodeName === 'TD') {
-            event.target.style.background = color;
-        }
-    });
-
-    for (let i = 0; i < height; i++) {
-        let row = document.createElement('tr');
-        for (let j = 0; j < width; j++) {
-            const data = document.createElement('td');
-            row.appendChild(data);
-        }
-        pixelCanvas.appendChild(row);
+  const pixelCanvas = document.querySelector('#pixelCanvas');
+  pixelCanvas.addEventListener('click', function (event) {
+    if (event.target.nodeName === 'TD') {
+      event.target.style.background = color;
     }
+  });
 
+  for (let i = 0; i < height; i++) {
+    let row = document.createElement('tr');
+    for (let j = 0; j < width; j++) {
+      const data = document.createElement('td');
+      row.appendChild(data);
+    }
+    pixelCanvas.appendChild(row);
+  }
 }
+
+const colorPicker = document.querySelector('#colorPicker');
+let color = document.querySelector('#colorPicker').value;
+colorPicker.addEventListener('input', function () {
+  color = document.querySelector('#colorPicker').value;
+});
+
+const sizePicker = document.querySelector('#sizePicker');
+sizePicker.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const height = sizePicker.height.value;
+  const width = sizePicker.width.value;
+  eraseGrid();
+  makeGrid(height, width);
+});
